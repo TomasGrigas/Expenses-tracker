64363e6b4a7024d95dd11c6fc57ffa53daf42fdb
TomasGrigas <tomas.grigas@icloud.com>
2023-01-04 19:47:44 +0200

    addes styled-components

--- client/src/pages/Expenses/Expenses.jsx
+++ client/src/pages/Expenses/Expenses.jsx
@@ -2,6 +2,23 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { LOGGED_IN_USER } from "../../constants/constants";
 import { API_URL } from "../../constants/constants";
+import styled from 'styled-components'
+
+
+const ExpensesList = styled.ul`
+    list-styled = none
+
+`;
+const ExpensesListItem = styled.li`
+    display: flex;
+    justify-content: center;
+    gap: 30px;
+    margin: 10px;
+    padding: 5px;
+    color: white;
+    background-color: grey;
+    
+`;
 
 
 
@@ -10,7 +27,7 @@ export const Expenses = () => {
     const [isLoading,setIsLoading] = useState([]);
 
         useEffect(()=>{
-            fetch(`${API_URL}/expenses?userId=${LOGGED_IN_USER}`)
+            fetch(`${API_URL}/expenses?userId=${LOGGED_IN_USER.id}`)
             .then(res=>res.json())
             .then(data => {
                 setExpenses(data);
@@ -23,9 +40,14 @@ export const Expenses = () => {
         }
 
         return (
-            <div>
-                {expenses.map((exp) => <div key={exp.id}>{exp.amount}</div>)}
-            </div>
+            <ExpensesList>
+                {expenses.map((exp) => (
+                    <ExpensesListItem  key={exp.id}>
+                        <span>{exp.type}</span>
+                        <span>€{exp.amount}</span>
+                    </ExpensesListItem >
+                ))}
+            </ExpensesList>
         )
     }
 
