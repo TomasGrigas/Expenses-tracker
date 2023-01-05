cb1b72fc5a2a82650cf116164a7ebee5bb2e1af2
TomasGrigas <tomas.grigas@icloud.com>
2023-01-05 19:03:11 +0200

    added env variables REACT

--- app/app.js
+++ app/app.js
@@ -2,6 +2,8 @@ const cors = require('cors');
 const express = require('express');
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 
 const app = express();
 
@@ -9,11 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 const mysqlConfig = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'zalgiris',
-    database: 'expenses_tracker',
-    port: 3306
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 };
 
 const connection = mysql.createConnection(mysqlConfig);
